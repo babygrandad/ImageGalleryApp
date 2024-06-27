518f6fabc32985672c63df9fb4fef5bed7fa9ecc
babygrandad <relebogilenkotswe@gmail.com>
2024-06-27 11:00:15 +0200

    cleaned up code on DTOs And Mappers

--- api/DTOs/Comment/GetCommentDTO.cs
+++ api/DTOs/Comment/GetCommentDTO.cs
@@ -12,7 +12,6 @@ namespace api.DTOs.Comment
         public DateTime CommentDate { get; set; }
         public DateTime? LastUpdate { get; set; }
         public bool IsEdited { get; set; }
-        //public string UserName { get; set; }
         public string FullName { get; set; }
         // Other properties as needed
     }
