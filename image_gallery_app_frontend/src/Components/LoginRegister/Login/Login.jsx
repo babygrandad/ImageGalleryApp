b10a9ba1dea729ebe7ae7906a2d6e61a795aa54e
babygrandad <relebogilenkotswe@gmail.com>
2024-07-03 18:19:50 +0200

    login and register page is made close to lindo's design

--- image_gallery_app_frontend/src/Components/LoginRegister/Login/Login.jsx
+++ image_gallery_app_frontend/src/Components/LoginRegister/Login/Login.jsx
@@ -1,36 +1,46 @@
 import React from 'react'
 import LoginRegister from '../LoginRegister.module.css'
+import LoginStyle from './Login.module.css'
 
 function Login() {
   return (
-    <div className={`${LoginRegister.loginRegisterContainer} ${LoginRegister.Container}`}>
-      <aside>
-      <div className={LoginRegister.formText}>
-            <h3>Login</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit?</p>
+    <div className={LoginStyle.LoginContainer}>
+      <form className={LoginStyle.loginForm}>
+        <div className={`${LoginRegister.formText} ${LoginStyle.formText}`}>
+          <h3>Image Gallery App</h3>
+          <h3>Log in</h3>
+        </div>
+        <div className={LoginRegister.formInputsWrapper}>
+          <div className={LoginRegister.formInfoContainer}>
+            <label htmlFor="loginEmail" className={LoginRegister.formLable}>Email</label>
+            <div className={LoginStyle.formFieldWrapper}>
+              <i class="fas fa-user"></i>
+              <input id='loginEmail' type='text' placeholder='Enter Email'  className={`${LoginStyle.inputField} inputField`} />
+            </div>
+            <span id='loginEmailError' className={LoginRegister.errorText}>Email field cannot be left empty.</span>
           </div>
-          <div>
-            <form className={LoginRegister.loginRegisterForm} autoComplete='false'>
-              <div className={LoginRegister.formFieldContainer}>
-                <lable for='emailAddress' >Email Address</lable>
-                <input className='inputField' type='email' id='emailAddress' placeholder='Enter Email' />
+          <div className={LoginRegister.formInfoContainer}>
+            <label htmlFor="loginPassword" className={LoginRegister.formLable}>Password</label>
+            <div className={LoginStyle.formFieldWrapper}>
+              <i class="fas fa-lock"></i>
+              <input id='loginPassword' type='password' placeholder='Enter Password' className={`${LoginStyle.inputField} inputField`} />
+            </div>
+            <span id='loginPasswordError' className={LoginRegister.errorText}>.-.</span>
+            <div className={LoginStyle.checkContainer}>
+              <div>
+              <input type='checkbox'/> <span className={LoginStyle.rememberText}>Remember me?</span>
               </div>
-              <div className={LoginRegister.formFieldContainer}>
-                <lable for='emailAddress'>Password</lable>
-                <input className='inputField' type='password' id='password' placeholder='Enter Password' />
-              </div>
-              <div className={`${LoginRegister.formFieldContainer} ${LoginRegister.buttonContainer}`}>
-                <input className={`${LoginRegister.formButton} button`} type="button" name="register" id="register" value={"Login"} />
-              </div>
-              <div className={`${LoginRegister.formFieldContainer} ${LoginRegister.alternateLogin}`}>
-                <p>Don't have an account? <a href='/'>Register</a></p>
-              </div>
-            </form>
+              <a href="/forgotPassword" className={LoginStyle.forgotPassword}>Forgot Password?</a>
+            </div>
           </div>
-        </aside>
-        <main>
-
-        </main>
+          <div className={`${LoginRegister.formInfoContainer} ${LoginRegister.buttonContainer}`}>
+            <button id='LoginButton' className={`${LoginRegister.formButton} button`}>Login</button>
+          </div>
+          <div className={LoginRegister.formInfoContainer}>
+            <p className={LoginRegister.loginRegisterQuestion}>New to this platform? <a className={LoginRegister.loginRegisterLink} href='/'>Register</a> Here</p>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
--- image_gallery_app_frontend/src/Components/LoginRegister/Register/Register.jsx
+++ image_gallery_app_frontend/src/Components/LoginRegister/Register/Register.jsx
@@ -1,44 +1,63 @@
 import React from 'react'
 import LoginRegister from '../LoginRegister.module.css'
+import RegisterStyle from './Register.module.css'
 
 const Register = () => {
   return (
-    <div className={`${LoginRegister.loginRegisterContainer} ${LoginRegister.Container}`}>
-      <aside>
-          <div className={LoginRegister.formText}>
-            <h3>Register Profile</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit?</p>
-          </div>
-          <div>
-            <form className={LoginRegister.loginRegisterForm} autoComplete='false'>
-            <div className={LoginRegister.formFieldContainer}>
-                <lable for='fullName'>Full Name</lable>
-                <input className='inputField' type='text' id='fullName' placeholder='Enter Name' />
+    <div className={RegisterStyle.registerContainer}>
+      <div className={RegisterStyle.registerWrapper}>
+        <div className={RegisterStyle.aside}>
+          <form className={RegisterStyle.registerForm}>
+            <div className={`${LoginRegister.formText} ${RegisterStyle.formText}`}>
+              <h3>Image Gallery App</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, quidem!</p>
+            </div>
+            <div className={LoginRegister.formInputsWrapper}>
+              <div className={LoginRegister.formInfoContainer}>
+                <label htmlFor="registerFullName" className={LoginRegister.formLable}>Full Name</label>
+                <div className={RegisterStyle.formFieldWrapper}>
+                  <input id='registerFullName' type='text' placeholder='Enter Name' className={`${RegisterStyle.inputField} inputField`} />
+                </div>
+                <span id='registerFullNameError' className={LoginRegister.errorText}>Full Name field cannot be left empty.</span>
               </div>
-              <div className={LoginRegister.formFieldContainer}>
-                <lable for='emailAddress' >Email Address</lable>
-                <input className='inputField' type='email' id='emailAddress' placeholder='Enter Email' />
+              <div className={LoginRegister.formInfoContainer}>
+                <label htmlFor="registerEmail" className={LoginRegister.formLable}>Email Address</label>
+                <div className={RegisterStyle.formFieldWrapper}>
+                  <input id='registerEmail' type='email' placeholder='Enter Email' className={`${RegisterStyle.inputField} inputField`} />
+                </div>
+                <span id='registerEmailError' className={LoginRegister.errorText}>Email field cannot be left empty.</span>
               </div>
-              <div className={LoginRegister.formFieldContainer}>
-                <lable for='emailAddress'>Password</lable>
-                <input className='inputField' type='password' id='password' placeholder='Enter Password' />
+              <div className={LoginRegister.formInfoContainer}>
+                <label htmlFor="registerPassword" className={LoginRegister.formLable}>Password</label>
+                <div className={RegisterStyle.formFieldWrapper}>
+                  <input id='registerPassword' type='password' placeholder='Enter Password' className={`${RegisterStyle.inputField} inputField`} />
+                </div>
+                <span id='registerPasswordError' className={LoginRegister.errorText}>Password field cannot be left empty.</span>
               </div>
-              <div className={LoginRegister.formFieldContainer}>
-                <lable for='confirmPassword'>Email Address</lable>
-                <input className='inputField' type='password' id='confirmPassword' placeholder='Enter Password' />
+              <div className={LoginRegister.formInfoContainer}>
+                <label htmlFor="registerConfirmPassword" className={LoginRegister.formLable}>Confirm Password</label>
+                <div className={RegisterStyle.formFieldWrapper}>
+                  <input id='registerConfirmPassword' type='password' placeholder='Enter Password' className={`${RegisterStyle.inputField} inputField`} />
+                </div>
+                <span id='registerConfirmPasswordError' className={LoginRegister.errorText}>Password field cannot be left empty.</span>
               </div>
-              <div className={`${LoginRegister.formFieldContainer} ${LoginRegister.buttonContainer}`}>
-                <input className={`${LoginRegister.formButton} button`} type="button" name="register" id="register" value={"Register"} />
+              <div className={`${LoginRegister.formInfoContainer} ${LoginRegister.buttonContainer}`}>
+                <button id='LoginButton' className={`${LoginRegister.formButton} button`}>Register</button>
               </div>
-              <div className={`${LoginRegister.formFieldContainer} ${LoginRegister.alternateLogin}`}>
-                <p>Already have an account? <a href='/login'>Login</a></p>
+              <div className={LoginRegister.formInfoContainer}>
+                <p className={LoginRegister.loginRegisterQuestion}>Already have an account? <a className={LoginRegister.loginRegisterLink} href='/login'>Login</a> Here</p>
               </div>
-            </form>
-          </div>
-        </aside>
-        <main>
-
-        </main>
+              <div className={LoginRegister.socialButtonsWrapper}>
+                <button id='RegisterToGoogleButton' className={`${LoginRegister.formButton} ${LoginRegister.socialButton} button`}><i class="fab fa-google"></i> Sign in with Google</button>
+                <button id='RegisterToFacebookButton' className={`${LoginRegister.formButton} ${LoginRegister.socialButton} button`}><i class="fab fa-facebook"></i> Sign in with Facebook</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className={RegisterStyle.main}>
+          
+        </div>
+      </div>
     </div>
   )
 }
