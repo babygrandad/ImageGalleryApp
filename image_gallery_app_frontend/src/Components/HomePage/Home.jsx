3de7c8520448030186c3dbabd930975e5403e27c
babygrandad <relebogilenkotswe@gmail.com>
2024-07-12 11:43:10 +0200

    About to add images to the contentsection - Forgot password and reset password are good.

--- image_gallery_app_frontend/src/Components/HomePage/Home.jsx
+++ image_gallery_app_frontend/src/Components/HomePage/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import CommonStyles from '../CommonComponents/CommonComponents.module.css'
 import SideMenu from '../SideMenu/SideMenu'
-import ContentContainer from '../ContentContainer/ContentContainer'
 import ImageSection from '../ImageSection/ImageSection'
 
 
