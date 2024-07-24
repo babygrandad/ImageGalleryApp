df725f84c36ee66c8d56462c4cd53692051f5c08
babygrandad <relebogilenkotswe@gmail.com>
2024-07-24 14:38:09 +0200

    categories are now pulling in from the back end

--- image_gallery_app_frontend/src/Components/UploadSection/UploadSection.jsx
+++ image_gallery_app_frontend/src/Components/UploadSection/UploadSection.jsx
@@ -1,37 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import UploadStyle from './UploadSection.module.css'
 import FormStyles from '../CommonComponents/Forms/FormStyles.module.css'
 import LoginRegisterSubmitButton from '../LoginRegister/Subcomponents/LoginRegisterSubmitButton'
 import ImageUploader from '../SubComponents/ImageUploader';
+import BASE_URL from '../../config';
 
 
 function UploadSection(props) {
 
-
-	const [selectedFile, setSelectedFile] = useState(null);
 	const [errorMessage, setErrorMessage] = useState(null);
-  
-	const handleSubmit = (e) => {
-	  e.preventDefault();
+	const [categories, setCategories] = useState([]);
 
-	  console.log('name', e.target.name)
-  
-	  // Handle file upload here (placeholder)
-	//  if (!selectedFile) {
-	//	setErrorMessage('Please select a file to upload');
-	//	return;
-	//  }
-  
-	  // Implement logic to send the form data and file to your server
-	  // You can use libraries like axios or Fetch API
-  
-	  // Clear error message and selected file after successful upload (placeholder)
-	//  setErrorMessage(null);
-	//  setSelectedFile(null);
-	}
-  
-	const handleFileChange = (event) => {
-	  setSelectedFile(event.target.files[0]);
+	// retrieves and sets the dropdown options for the Categories
+	useEffect(() => {
+		const fetchCategories = async () => {
+			try {
+				const response = await axios.get(`${BASE_URL}/category`);
+				const categoryNames = response.data.map((item) => capitalizeFirstLetter(item.categoryName));
+				setCategories(categoryNames)
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		fetchCategories();
+	}, []);
+
+	// Capitalizez the first letter of every category option 
+	const capitalizeFirstLetter = (string) => {
+		return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('name', e.target.name)
 	}
 
 	return (
@@ -47,17 +49,10 @@ function UploadSection(props) {
 					<div className={FormStyles.formInfoContainer}>
 						<label htmlFor="imageCategory" className={FormStyles.formLable}>Image Category</label>
 						<select type="select" name="imageCategory" id="imageCategory" className={`${FormStyles.inputField} inputField`} >
-							<option value="">--Please choose an option--</option>
-							<option value="Option 1">Option 1</option>
-							<option value="Option 2">Option 2</option>
-							<option value="Option 3">Option 3</option>
-							<option value="Option 4">Option 4</option>
-							<option value="Option 5">Option 5</option>
-							<option value="Option 6">Option 6</option>
-							<option value="Option 7">Option 7</option>
-							<option value="Option 8">Option 8</option>
-							<option value="Option 9">Option 9</option>
-							<option value="Option 10">Option 10</option>
+							<option value="" >--Please choose an option--</option>
+							{categories.map((category, index) => (
+								<option key={index} value={category}>{category}</option>
+							))}
 						</select>
 					</div>
 					<div className={FormStyles.formInfoContainer}>
