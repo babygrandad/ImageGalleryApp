1c1ad6b6fd6f7d94c76ef1ab43b2d5eefe25e5bf
babygrandad <relebogilenkotswe@gmail.com>
2024-08-12 08:43:40 +0200

    Side menu links are now woking perfectly

--- image_gallery_app_frontend/src/Components/SideMenu/SideMenu.jsx
+++ image_gallery_app_frontend/src/Components/SideMenu/SideMenu.jsx
@@ -1,42 +1,43 @@
-import React from 'react'
-import SideMenuStyles from './SideMenu.module.css'
-
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import SideMenuStyles from './SideMenu.module.css';
 
 function SideMenu() {
-	return (
-		<aside className={SideMenuStyles.sideMenu}>
-			<div className={SideMenuStyles.logoContainer}>
-				<a href="/home" className={`${SideMenuStyles.topLogoLink} ${SideMenuStyles.sideMenuLinks}`}>
-				<img src="https://i.ibb.co/3Mr2PNB/Logo-1-white.png" alt="Logo-1-white" className={`${SideMenuStyles.topLogoImage}`}/>
-				</a>
-			</div>
-			<div className={SideMenuStyles.menuContainer}>
-				<ul className={SideMenuStyles.sideMenuList}>
-					<li className={`${SideMenuStyles.sideMenuListItem} ${SideMenuStyles.active}`/*remeber to change this "${SideMenuStyles.active}" when coming to hooks and use states Angela will show you*/}>
-						<a className={SideMenuStyles.sideMenuLinks}>
-							<span className={`${SideMenuStyles.sideMenuIcons} material-symbols-outlined`}>home</span>Home
-						</a>
-					</li>
-					<li className={SideMenuStyles.sideMenuListItem}>
-						<a className={SideMenuStyles.sideMenuLinks}>
-							<span className={`${SideMenuStyles.sideMenuIcons} material-symbols-outlined`}>add_a_photo</span>Image Upload
-						</a>
-					</li>
-					<li className={SideMenuStyles.sideMenuListItem}>
-						<a className={SideMenuStyles.sideMenuLinks}>
-							<span className={`${SideMenuStyles.sideMenuIcons} material-symbols-outlined`}>person</span>Profile
-						</a>
-					</li>
-				</ul>
-			</div>
-			<div className={SideMenuStyles.menuLogoutContainer}>
+    const location = useLocation();
 
-				<a href="/" className={`${SideMenuStyles.menuLogoutLink} ${SideMenuStyles.sideMenuLinks}`}>
-					<span className={`${SideMenuStyles.sideMenuIcons} material-symbols-outlined`}>logout</span> Logout
-				</a>
-			</div>
-		</aside>
-	)
+    return (
+        <aside className={SideMenuStyles.sideMenu}>
+            <div className={SideMenuStyles.logoContainer}>
+                <a href="/home" className={`${SideMenuStyles.topLogoLink} ${SideMenuStyles.sideMenuLinks}`}>
+                    <img src="https://i.ibb.co/3Mr2PNB/Logo-1-white.png" alt="Logo-1-white" className={`${SideMenuStyles.topLogoImage}`} />
+                </a>
+            </div>
+            <div className={SideMenuStyles.menuContainer}>
+                <ul className={SideMenuStyles.sideMenuList}>
+                    <li className={`${SideMenuStyles.sideMenuListItem} ${location.pathname === '/home' ? SideMenuStyles.active : ''}`}>
+                        <a href='/home' className={SideMenuStyles.sideMenuLinks}>
+                            <span className={`${SideMenuStyles.sideMenuIcons} material-symbols-outlined`}>home</span>Home
+                        </a>
+                    </li>
+                    <li className={`${SideMenuStyles.sideMenuListItem} ${location.pathname === '/upload' ? SideMenuStyles.active : ''}`}>
+                        <a href='/upload' className={SideMenuStyles.sideMenuLinks}>
+                            <span className={`${SideMenuStyles.sideMenuIcons} material-symbols-outlined`}>add_a_photo</span>Image Upload
+                        </a>
+                    </li>
+                    <li className={`${SideMenuStyles.sideMenuListItem} ${location.pathname === '/profile' ? SideMenuStyles.active : ''}`}>
+                        <a href='/profile' className={SideMenuStyles.sideMenuLinks}>
+                            <span className={`${SideMenuStyles.sideMenuIcons} material-symbols-outlined`}>person</span>Profile
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className={SideMenuStyles.menuLogoutContainer}>
+                <a href="/" className={`${SideMenuStyles.menuLogoutLink} ${SideMenuStyles.sideMenuLinks}`}>
+                    <span className={`${SideMenuStyles.sideMenuIcons} material-symbols-outlined`}>logout</span> Logout
+                </a>
+            </div>
+        </aside>
+    );
 }
 
-export default SideMenu
+export default SideMenu;
